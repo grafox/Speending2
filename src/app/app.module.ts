9e3c4ce3715510e13bc5cd7b5b07ae1cd00c8241
grafox <g_fox_fts@yahoo.com>
2017-09-18 11:32:01 +0200

    setp one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,58 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { ForgotPage } from '../pages/forgot/forgot';
+import { RegisterPage } from '../pages/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Auth } from '../providers/auth/auth';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5MPp8ntjND8yNW_DVxQ1_drpVd42uvcI",
+  authDomain: "grafoxdev.firebaseapp.com",
+  databaseURL: "https://grafoxdev.firebaseio.com",
+  projectId: "grafoxdev",
+  storageBucket: "grafoxdev.appspot.com",
+  messagingSenderId: "1018202227947"
+};
+
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+  ],
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    ForgotPage,
+    RegisterPage
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    LoginPage,
+    ForgotPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Auth
   ]
 })
-export class AppModule {}
+export class AppModule { }
